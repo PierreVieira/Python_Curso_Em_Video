1b0ee602978afec09249a19b6aa71536c473c6a0
PierreVieira <pierrevieiraggg@gmail.com>
2019-12-24 18:10:15 -0300

    [+] Random and mixer

--- 23.py
+++ 23.py
@@ -1,3 +1,8 @@
 """
 Exercício Python 023: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
 """
+numero = int(input('Informe um número: '))
+print(f'Unidade: {numero%10}')
+print(f'Dezena: {(numero%100)//10}')
+print(f'Centena: {(numero%1000)//100}')
+print(f'Milhar: {(numero%10000)//1000}')
