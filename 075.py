f9c4eb2f7a5bbc7820c4244ea0c13f1438fdf75c
PierreVieira <pierrevieiraggg@gmail.com>
2020-01-01 13:58:03 -0300

    [+] Tupples

--- 075.py
+++ 075.py
@@ -5,5 +5,5 @@ mostre a listagem de números gerados e também indique o menor e o maior valor 
 
 from random import sample
 tupla_aleatoria = tuple(sample(range(11), 5))
-print(f'Os valores sorteados foram: {str(tupla_aleatoria)}')
+print(f'Os valores sorteados foram: {str(tupla_aleatoria)[1:len(str(tupla_aleatoria))-1:]}')
 print(f'Maior: {max(tupla_aleatoria)}\nMenor: {min(tupla_aleatoria)}')
